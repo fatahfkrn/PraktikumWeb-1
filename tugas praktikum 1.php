69899a7e8e40e3555041a12022719a5b9f2f4384
fatahfkrn <92284251+fatahfkrn@users.noreply.github.com>
2022-02-27 19:28:35 +0700

    Add files via upload

---
+++ tugas praktikum 1.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Praktikum 01</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+      <?php
+      $ns1 = ['id' => 1, 'nim' => '01101', 'uts' => 80, 'uas' => 84, 'tugas' => 78];
+      $ns2 = ['id' => 2, 'nim' => '01121', 'uts' => 70, 'uas' => 50, 'tugas' => 68];
+      $ns3 = ['id' => 3, 'nim' => '01130', 'uts' => 60, 'uas' => 86, 'tugas' => 70];
+      $ns4 = ['id' => 4, 'nim' => '01134', 'uts' => 90, 'uas' => 91, 'tugas' => 82];
+
+      $ar_nilai = [$ns1, $ns2, $ns3, $ns4];
+      ?>
+      <center>
+            <h1 class="text-align-center">Daftar Nilai Siswa</h1>
+            <table width="100%" class="table table-bordered container">
+                  <thead class="table-dark">
+                        <tr>
+                              <th>No</th>
+                              <th>NIM</th>
+                              <th>UTS</th>
+                              <th>UAS</th>
+                              <th>Tugas</th>
+                              <th>Nilai Akhir</th>
+                        </tr>
+                  </thead>
+                  <tbody>
+                        <?php
+                        $nomor = 1;
+                        foreach ($ar_nilai as $ns) {
+                              echo '<tr><td>' . $nomor . '</td>';
+                              echo '<td>' . $ns['nim'] . '</td>';
+                              echo '<td>' . $ns['uts'] . '</td>';
+                              echo '<td>' . $ns['uas'] . '</td>';
+                              echo '<td>' . $ns['tugas'] . '</td>';
+                              $nilai_akhir = ($ns['uts'] + $ns['uas'] + $ns['tugas']) / 3;
+                              echo '<td>' . number_format($nilai_akhir, 2, ',', '.') . '</td>';
+                              echo '<tr/>';
+                              $nomor++;
+                        }
+                        ?>
+                  </tbody>
+            </table>
+      </center>
+
+</body>
+
+</html>
